e445e9d130828c638ce2cf0f6e758bbbcb12f1e3
osman28tr <osmantonbul2001@gmail.com>
2022-07-08 18:23:39 +0300

    Primary Key'lerin belirlenmesi.

--- EntityLayer/Concrete/SocialMedia.cs
+++ EntityLayer/Concrete/SocialMedia.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntityLayer.Concrete
 {
     public class SocialMedia
     {
+        [Key]
         public int SocialMediaId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
