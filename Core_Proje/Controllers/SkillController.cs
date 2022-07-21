bc2043cc406f70777e8fd9dbf8ff4d16e01ef531
osman28tr <osmantonbul2001@gmail.com>
2022-07-21 18:06:19 +0300

    Yeteneklerin listelenmesi (admin)

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
 	public class SkillController : Controller
 	{
+		SkillManager skillManager = new SkillManager(new EfSkillDal());
 		public IActionResult Index()
 		{
-			return View();
+			var values = skillManager.TGetList();
+			return View(values);
 		}
 	}
 }
