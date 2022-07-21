9215eca08799eb19ac8e28f7791ea63222d5ad3f
osman28tr <osmantonbul2001@gmail.com>
2022-07-21 20:13:04 +0300

    Deneyimleri listeleme işlemi gerçekleştirildi. (admin)

---
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+	public class ExperienceController : Controller
+	{
+		ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal()); 
+		public IActionResult Index()
+		{
+			ViewBag.value1 = "Deneyim listesi";
+			ViewBag.value2 = "Deneyimler";
+			ViewBag.value3 = "Deneyim listesi";
+			var values = experienceManager.TGetList();
+			return View(values);
+		}
+	}
+}
