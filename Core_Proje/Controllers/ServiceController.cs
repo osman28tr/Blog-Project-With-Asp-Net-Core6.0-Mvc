743fb60276742def9fe464f0ae12a62fc44fa3b9
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 00:48:40 +0300

    Servis ekleme ve güncelleme işlemleri gerçekleştirildi.

--- Core_Proje/Controllers/ServiceController.cs
+++ Core_Proje/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -15,5 +16,40 @@ namespace Core_Proje.Controllers
             var values = serviceManager.TGetList();
             return View(values);
         }
-    }
+		[HttpGet]
+		public IActionResult AddService()
+		{
+			ViewBag.value1 = "Hizmet listesi";
+			ViewBag.value2 = "Hizmetler";
+			ViewBag.value3 = "Hizmet ekleme";
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddService(Service service)
+		{
+			serviceManager.TAdd(service);
+			return RedirectToAction("Index");
+		}
+		public IActionResult DeleteService(int id)
+		{
+			var serviceValue = serviceManager.TGetById(id);
+			serviceManager.TDelete(serviceValue);
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public IActionResult EditService(int id)
+		{
+			ViewBag.value1 = "Hizmet listesi";
+			ViewBag.value2 = "Hizmetler";
+			ViewBag.value3 = "Hizmet düzenleme";
+			var serviceValue = serviceManager.TGetById(id);
+			return View(serviceValue);
+		}
+		[HttpPost]
+		public IActionResult EditService(Service service)
+		{
+			serviceManager.TUpdate(service);
+			return RedirectToAction("Index");
+		}
+	}
 }
