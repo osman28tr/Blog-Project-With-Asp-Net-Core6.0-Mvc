6d9f096b21b3d5cbc833d72d277b99174692f293
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 00:26:08 +0300

    About işlemleri gerçekleştirildi.

---
+++ Core_Proje/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+		AboutManager aboutManager = new AboutManager(new EfAboutDal());
+		[HttpGet]
+		public IActionResult EditAbout()
+		{
+			ViewBag.value1 = "Düzenleme";
+			ViewBag.value2 = "Hakkımda";
+			ViewBag.value3 = "Hakkımda Sayfası";
+			var aboutValue = aboutManager.TGetById(2);
+			return View(aboutValue);
+		}
+		[HttpPost]
+		public IActionResult EditAbout(About about)
+		{
+			aboutManager.TUpdate(about);
+			return RedirectToAction("Index", "Default");
+		}
+	}
+}
