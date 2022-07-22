18aa025fefb2006653d2a9b0162e53e3f2f077d6
osman28tr <osmantonbul2001@gmail.com>
2022-07-22 18:16:45 +0300

    Portfolio ekleme işlemi gerçekleştirildi.

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -14,6 +15,20 @@ namespace Core_Proje.Controllers
             ViewBag.value3 = "Proje listesi";
             var values = portfolioManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.value1 = "Proje listesi";
+            ViewBag.value2 = "Projelerim";
+            ViewBag.value3 = "Proje ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {      
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
         }
     }
 }
