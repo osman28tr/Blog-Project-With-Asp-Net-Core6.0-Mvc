a21943cf9c9d9b3b679f4e886a38a680dd26599c
osman28tr <osmantonbul2001@gmail.com>
2022-07-22 17:34:29 +0300

    Portfolio listeleme işlemi gerçekleştirildi.(admin)

--- EntityLayer/Concrete/Portfolio.cs
+++ EntityLayer/Concrete/Portfolio.cs
@@ -17,7 +17,7 @@ namespace EntityLayer.Concrete
         public string ProjectUrl { get; set; }
         public string Platform { get; set; }
         public string Price { get; set; }
-        public string Status { get; set; }
+        public bool Status { get; set; }
         public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
