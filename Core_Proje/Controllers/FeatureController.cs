8ab6757948484fa5155c6d59d2deb9a0fa78972f
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 00:16:03 +0300

    Feature düzenleme işlemleri gerçekleştirildi.

--- Core_Proje/Controllers/FeatureController.cs
+++ Core_Proje/Controllers/FeatureController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
 	public class FeatureController : Controller
 	{
-		public IActionResult Index()
+		FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        [HttpGet]
+		public IActionResult EditFeature()
 		{
-			return View();
+			ViewBag.value1 = "Düzenleme";
+			ViewBag.value2 = "Öne Çıkanlar";
+			ViewBag.value3 = "Öne Çıkan Sayfası";
+			var featureValue = featureManager.TGetById(1);
+			return View(featureValue);
+		}
+		[HttpPost]
+		public IActionResult EditFeature(Feature feature)
+		{
+			featureManager.TUpdate(feature);
+			return RedirectToAction("Index","Default");
 		}
 	}
 }
