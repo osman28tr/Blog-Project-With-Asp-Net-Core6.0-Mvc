a6def6549ab4c59a06da451bdd32979fcc9ec338
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 00:37:25 +0300

    Servis listeleme işlemleri gerçekleştirildi.

---
+++ Core_Proje/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            ViewBag.value1 = "Hizmet listesi";
+            ViewBag.value2 = "Hizmetler";
+            ViewBag.value3 = "Hizmet listesi";
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+    }
+}
