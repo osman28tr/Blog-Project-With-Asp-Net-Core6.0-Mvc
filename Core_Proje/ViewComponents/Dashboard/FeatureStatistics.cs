c32911fdeaffc771d4d7245ce090ae81f21404a6
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 19:03:13 +0300

    Yeni temada dashboard düzenlemeleri yapıldı.

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
