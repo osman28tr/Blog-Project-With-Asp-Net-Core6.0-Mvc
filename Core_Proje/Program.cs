d6871acd4018010f43f47152deec684bdd94b48d
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 14:21:29 +0300

    Yeni temada layout düzenlemeleri yapıldı.

--- Core_Proje/Program.cs
+++ Core_Proje/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Test}/{action=Index}/{id?}");
 
 app.Run();
