714518faf999c739059c0e42ad3ce33f0d4bee65
osman28tr <osmantonbul2001@gmail.com>
2022-07-23 19:19:55 +0300

    İstatistikler yazildi.

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.ViewComponents.Dashboard
 {
     public class FeatureStatistics:ViewComponent
     {
+        Context context = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.static1 = context.Skills.Count();
+            ViewBag.static2 = context.Messages.Where(x => x.Status == false).Count();
+            ViewBag.static3 = context.Messages.Where(x => x.Status == true).Count();
+            ViewBag.static4 = context.Experiences.Count();
+
             return View();
         }
     }
