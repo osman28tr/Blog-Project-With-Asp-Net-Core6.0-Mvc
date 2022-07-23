b0430d2f1d5a00f76abb062f6d22f9f749742f3c
osman28tr <osmantonbul2001@gmail.com>
2022-07-24 01:11:21 +0300

    Dashboard istatistikler devam

---
+++ Core_Proje/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class StatisticsDashboard2:ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.value1 = context.Portfolios.Where(x => x.Value == 100).Count();
+            ViewBag.value2 = context.Messages.Count();
+            ViewBag.value3 = context.Services.Count();
+
+            return View();
+        }
+    }
+}
