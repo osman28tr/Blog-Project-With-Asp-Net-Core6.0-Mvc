d4d36decc67dcf68e85877ea9f28a2aca573f18d
osman28tr <osmantonbul2001@gmail.com>
2022-07-25 00:10:25 +0300

    Writer için yeni tema ekleme ve ilgili area oluşturma işlemi gerçekleştirildi.

--- Core_Proje/Program.cs
+++ Core_Proje/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Test}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
