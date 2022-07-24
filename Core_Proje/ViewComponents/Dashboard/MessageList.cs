030e8d7031319d9149dad87b622f1d999792c6bd
osman28tr <osmantonbul2001@gmail.com>
2022-07-24 13:34:21 +0300

    Dashboard mesaj paneli düzenlendi.

---
+++ Core_Proje/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class MessageList:ViewComponent
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
