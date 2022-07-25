3d9206c9c32275c8023d3115f678831e904739ed
osman28tr <osmantonbul2001@gmail.com>
2022-07-25 19:19:25 +0300

    Register formu eklendi.

---
+++ Core_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string p)
+        {
+            return View();
+        }
+    }
+}
