eedc4cd881fa50aaeb77ce8a0ed03a9620ee306f
osman28tr <osmantonbul2001@gmail.com>
2022-07-28 12:48:35 +0300

    Authorize islemi gerceklestirildi.

--- Core_Proje/Areas/Writer/Controllers/LoginController.cs
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,9 +13,28 @@ namespace Core_Proje.Areas.Writer.Controllers
         {
             _signInManager = signInManager;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel userLoginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(userLoginViewModel.UserName, userLoginViewModel.Password, true, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı kullanıcı adı veya şifre");
+                }
+            }
+            return View();
+        }
     }
 }
+//sifre 123456aA*
