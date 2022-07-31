9d387e996de36da387f995553fbe45008cee380a
osman28tr <osmantonbul2001@gmail.com>
2022-07-31 14:27:57 +0300

    Writer dashboard tarafında giris yapan kullanicinin ad ve soyad bilgileri getirildi.

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+        public DashboardController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.value = values.Name + " " + values.Surname;
             return View();
         }
     }
