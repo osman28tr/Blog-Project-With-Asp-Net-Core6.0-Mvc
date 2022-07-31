c973a63d2b8f513f1e451ce3a89844d0d9a66745
osman28tr <osmantonbul2001@gmail.com>
2022-07-31 15:25:41 +0300

    Writer dashboard istatistik verileri gösterildi.

---
+++ BusinessLayer/Concrete/StatisticManager.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class StatisticManager : IStatisticService
+    {
+        Context context = new Context();
+        public int DuyuruSayisi()
+        {
+            return context.Announcements.Count();
+        }
+
+        public int GelenMesajSayisi()
+        {
+            return 0;
+        }
+
+        public int ToplamKullaniciSayisi()
+        {
+            return 0;
+        }
+
+        public int ToplamYetenekSayisi()
+        {
+            return context.Skills.Count();
+        }
+    }
+}
--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace Core_Proje.Areas.Writer.Controllers
     public class DashboardController : Controller
     {
         private readonly UserManager<WriterUser> _userManager;
+        StatisticManager statisticManager = new StatisticManager();
         public DashboardController(UserManager<WriterUser> userManager)
         {
             _userManager = userManager;
@@ -16,6 +18,13 @@ namespace Core_Proje.Areas.Writer.Controllers
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.value = values.Name + " " + values.Surname;
+
+            //statistics
+            ViewBag.value1 = statisticManager.GelenMesajSayisi();
+            ViewBag.value2 = statisticManager.DuyuruSayisi();
+            ViewBag.value3 = statisticManager.ToplamKullaniciSayisi();
+            ViewBag.value4 = statisticManager.ToplamYetenekSayisi();
+
             return View();
         }
     }
