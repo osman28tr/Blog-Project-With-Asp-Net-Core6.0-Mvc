2fedbe26cf50f92f9b09a9fe6d81aa975b119fd4
osman28tr <osmantonbul2001@gmail.com>
2022-07-31 15:34:55 +0300

    Api ile hava durumu bilgisi cekildi.

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
@@ -18,6 +19,14 @@ namespace Core_Proje.Areas.Writer.Controllers
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.value = values.Name + " " + values.Surname;
+
+            
+            //weather api
+
+            string api = "14ad2aba611dbef9c504b82a127794c5";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.value6 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
 
             //statistics
             ViewBag.value1 = statisticManager.GelenMesajSayisi();
