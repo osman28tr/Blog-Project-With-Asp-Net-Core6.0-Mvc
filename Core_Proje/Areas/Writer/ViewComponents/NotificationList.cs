8f7768dd3200dcde42ae4b88f2ba61ae71ac502f
osman28tr <osmantonbul2001@gmail.com>
2022-07-31 17:10:19 +0300

    Writer kısmında navbar'da bildirimler kısmı düzenlendi.

---
+++ Core_Proje/Areas/Writer/ViewComponents/NotificationList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Areas.Writer.ViewComponents
+{
+    public class NotificationList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
