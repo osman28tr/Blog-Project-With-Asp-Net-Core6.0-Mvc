b19d2396a111ce7322a1c7d37e08d9b043389d69
osman28tr <osmantonbul2001@gmail.com>
2022-07-31 22:25:34 +0300

    Writer tarafında giris yapan kullanıcıya ait mesaj listesi getirildi.

--- Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class MessageController : Controller
     {
-        public IActionResult Index()
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        private readonly UserManager<WriterUser> _userManager;
+        public MessageController(UserManager<WriterUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index(string writerMail)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            writerMail = values.Email;
+            var messageList = writerMessageManager.GetListByFilter(writerMail);
+            return View(messageList);
         }
     }
 }
