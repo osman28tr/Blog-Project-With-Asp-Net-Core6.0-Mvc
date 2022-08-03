2a42489dd406f395712256bb56c180ee52825d25
osman28tr <osmantonbul2001@gmail.com>
2022-08-03 23:35:25 +0300

    Admin receiver message list islemleri gerceklestirildi.

---
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string mail = "admin@gmail.com";
+            var values = writerMessageManager.GetReceiverMessageList(mail);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string mail = "admin@gmail.com";
+            var values = writerMessageManager.GetSenderMessageList(mail);
+            return View(values);
+        }
+    }
+}
