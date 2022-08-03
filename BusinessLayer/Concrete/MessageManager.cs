16a8fd470be488b6b3f5641a81d7b0024d3e7fe8
osman28tr <osmantonbul2001@gmail.com>
2022-08-03 13:31:07 +0300

    Admin contact silme islemi gerceklestirildi.

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -23,12 +23,12 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Message t)
         {
-            throw new NotImplementedException();
+            _messageDal.Delete(t);
         }
 
         public Message TGetById(int id)
         {
-            throw new NotImplementedException();
+            return _messageDal.GetById(id);
         }
 
         public List<Message> TGetList()
