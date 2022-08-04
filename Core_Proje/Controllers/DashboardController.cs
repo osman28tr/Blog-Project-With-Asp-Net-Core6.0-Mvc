0fc973e03e93001e3d724b4d3047bedfd714d163
osman28tr <osmantonbul2001@gmail.com>
2022-08-04 20:26:46 +0300

    Dashboard üst menü hatası giderildi.

--- Core_Proje/Controllers/DashboardController.cs
+++ Core_Proje/Controllers/DashboardController.cs
@@ -6,9 +6,6 @@ namespace Core_Proje.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.value1 = "Dashboard";
-            ViewBag.value2 = "İstatistikler";
-            ViewBag.value3 = "İstatistik sayfası";
             return View();
         }
     }
