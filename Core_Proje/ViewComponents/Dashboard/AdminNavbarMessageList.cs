2001dc75ecab324dc779a9dfc82e1c1f2adf5f18
osman28tr <osmantonbul2001@gmail.com>
2022-08-04 19:30:00 +0300

    Admin üst menü mesaj listeleme işlemi gerçekleştirildi.

--- Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.ViewComponents.Dashboard
 {
-    public class AdminNavbarMessageList:ViewComponent
+    public class AdminNavbarMessageList : ViewComponent
     {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string mail = "admin@gmail.com";
+            var values = writerMessageManager.GetReceiverMessageList(mail).OrderByDescending(x => x.WriterMessageId).Take(3).ToList();
+            return View(values);
         }
     }
 }
