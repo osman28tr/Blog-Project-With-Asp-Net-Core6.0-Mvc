dd369a6537b3724eae3f6c029cf3fac21e55c993
osman28tr <osmantonbul2001@gmail.com>
2022-08-08 19:30:54 +0300

    Ajax ile deneyim verileri getirildi.

---
+++ Core_Proje/Controllers/ExperienceAjaxController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceAjaxController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            var values = JsonConvert.SerializeObject(experience);
+            return View(values);
+        }
+    }
+}
--- Core_Proje/Controllers/WriterUserController.cs
+++ Core_Proje/Controllers/WriterUserController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFrameworkCore;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -9,13 +10,20 @@ namespace Core_Proje.Controllers
     {
         WriterUserManager writerUserManager = new WriterUserManager(new EfWriterUserDal());
         public IActionResult Index()
-        {           
+        {
             return View();
         }
         public IActionResult ListUser()
         {
             var values = JsonConvert.SerializeObject(writerUserManager.TGetList());
             return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddUser(WriterUser writerUser)
+        {
+            writerUserManager.TAdd(writerUser);
+            var values = JsonConvert.SerializeObject(writerUser);
+            return View(values);
         }
     }
 }
