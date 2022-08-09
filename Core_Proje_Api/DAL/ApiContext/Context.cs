5483addd1ba8467643a2a5aaf4e227458f99000b
osman28tr <osmantonbul2001@gmail.com>
2022-08-09 16:15:33 +0300

    Api için gerekli Db ve Entity olusturuldu.

---
+++ Core_Proje_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using Core_Proje_Api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Proje_Api.DAL.ApiContext
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=DESKTOP-7LR4Q85\\" +
+				"SQLEXPRESS;Database=CoreProjeDB2;Integrated Security=true;");
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
