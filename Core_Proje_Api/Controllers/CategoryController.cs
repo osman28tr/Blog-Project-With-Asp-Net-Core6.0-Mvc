8ae2ce6ff441cd2b2f0497d64d17f971d778f900
osman28tr <osmantonbul2001@gmail.com>
2022-08-09 16:23:22 +0300

    Api ile bir get isteği ile verilerin listelenmesi işlemi gerçekleştirildi.

---
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje_Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult CategoryList()
+		{
+			var context = new Context();
+			return Ok(context.Categories.ToList());
+		}
+	}
+}
